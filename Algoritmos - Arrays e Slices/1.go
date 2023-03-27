47fa143b3c8407a186858bfe0ac996d2411a5c30
KaikeVitorino <kaike.vitorino@sempreceub;com>
2023-03-27 10:24:24 -0300

    Sala

---
+++ Algoritmos - Arrays e Slices/1.go
@@ -0,0 +1,6 @@
+package main
+
+func main() {
+	var numeros [3]int
+
+}
---
+++ Teste em aula/2.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	nomes := []string{"joao", "maria", "jose"}
+	fmt.Println(nomes)
+}
