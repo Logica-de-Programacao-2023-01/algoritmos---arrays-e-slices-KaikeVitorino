1bd1da4e9271d96bc5db071ebe25c91176cb0d40
KaikeVitorino <kaike.vitorino@sempreceub;com>
2023-03-27 10:47:25 -0300

    Sala

--- Teste em aula/4.go
+++ Teste em aula/4.go
@@ -1,5 +1,9 @@
 package main
 
+import "fmt"
+
 func main() {
-	numeros := [6]int{1, 2, 3, 4, 5, 6}
+	numeros := [6]float64{2, 4, 8, 16, 32, 64}
+	fmt.Printf("Media dos %d: ", numeros, (2+4+8+16+32+64)/6)
+
 }
